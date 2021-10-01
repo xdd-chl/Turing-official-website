1cbb7c6864af150258eefe551e069b7ef2913393
chlxdb <2214631535@qq.com>
2021-10-01 21:33:38 +0800

    chl

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         <Header>
           <Menu style={{ marginTop: "20px" }} className="nar" mode="horizontal" defaultSelectedKeys={['1']}>
             <NavLink to="/index">  <Menu.Item key={1}>首页</Menu.Item></NavLink>
-            <NavLink to="/leader">  <Menu.Item key={2}>领导视察</Menu.Item></NavLink>
+            <NavLink to="/leader">  <Menu.Item key={2}>领导视察12</Menu.Item></NavLink>
             <NavLink to="/index">  <Menu.Item key={3}>团队规模</Menu.Item></NavLink>
             <NavLink to="/index"> <Menu.Item key={4}>团队项目</Menu.Item></NavLink>
             <NavLink to="/index"> <Menu.Item key={5}>团队荣誉</Menu.Item></NavLink>
